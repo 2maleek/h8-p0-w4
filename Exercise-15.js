72cba00579116d7679a5b1a3e4b54ce52fbc1ca7
Malik Ibrahim Nur <56329434+2maleek@users.noreply.github.com>
2020-01-17 15:14:47 +0700

    Finish

--- Exercise-15.js
+++ Exercise-15.js
@@ -1,81 +1,33 @@
-function highestScore (students) { 
-	//grouping
-	var kelas = [students[0]["class"]];
-	console.log(students[0].class)
-	for(var i=1; i<students; i++){
-		var tambah = '';
-		console.log('luar')
-		for(var j=0; j<kelas.length; j++){
-			console.log('dalem')
-			if(kelas[j] !== students[i].class && tambah !== kelas[j]){
-				console.log('masuk')
-				tambah = students[i].class;
-			}
-		}
-		if(tambah !== '') {
-			kelas.push(tambah);
-		}
-	}
-	console.log(kelas);
-} 
-// TEST CASE 
-console.log(highestScore([ 
-	{ 
-		name: 'Dimitri', 
-		score: 90, 
-		class: 'foxes'
-	}, 
-	{ 
-		name: 'Alexei',
-		score: 85,
-		class: 'wolves' 
-	}, 
-	{ 
-		name: 'Sergei', 
-		score: 74, 
-		class: 'foxes' 
-	}, 
-	{ 
-		name: 'Anastasia', 
-		score: 78, 
-		class: 'wolves' 
-	} 
-])); 
-// { 
-	// foxes: { name: 'Dimitri', score: 90 }, 
-	// wolves: { name: 'Alexei', score: 85 } 
-// } 
-	console.log(highestScore([ 
-		{ 
-			name: 'Alexander', 
-			score: 100, 
-			class: 'foxes' 
-		}, 
-		{ 
-			name: 'Alisa', 
-	   	score: 76, 
-	   	class: 'wolves' 
-	   }, 
-	   { 
-	   	name: 'Vladimir', 
-	   	score: 92, 
-	   	class: 'foxes' 
-	   }, 
-	   { 
-	   	name: 'Albert', 
-	   	score: 71, 
-	   	class: 'wolves' 
-	   }, 
-	   { 
-	   	name: 'Viktor', 
-	   	score: 80, 
-	   	class: 'tigers' 
-	   } 
-])); 
-// { 
-	// foxes: { name: 'Alexander', score: 100 }, 
-	// wolves: { name: 'Alisa', score: 76 }, 
-	// tigers: { name: 'Viktor', score: 80 } 
-// } 
+function highestScore (students) {
+
+	var hasil = {};
+	for(var i = 0 ; i < students.length ; i ++){	
+    hasil[students[i].class] = { 	 
+     	name : students[i].name,
+      score : students[i].score
+    }
+  }
+  for(var i = 0 ; i < students.length ; i ++){
+  	 for(var j = 0 ; j < students.length ; j ++){
+if(students[i].class === students[j].class && students[i].score > students[j].score){
+ 	hasil[students[i].class].name = students[i].name;
+ 	hasil[students[i].class].score = students[i].score;
+ 	  }
+ 	 }
+ 	}return hasil;
+ }
+ // TEST CASE
+ console.log(highestScore([{name: 'Dimitri',score: 90,class: 'foxes'},{name: 'Alexei',score: 85,class: 'wolves'},{name: 'Sergei',score: 74,class: 'foxes'},{name: 'Anastasia',score: 78,class: 'wolves'}]));
+ // {
+ 	// foxes: { name: 'Dimitri', score: 90 },
+ 	// wolves: { name: 'Alexei', score: 85 }
+ 	// }
+ 	console.log(highestScore([{name: 'Alexander',score: 100,class: 'foxes'},{name: 'Alisa',score: 76,class: 'wolves'},{name: 'Vladimir',score: 92,class: 'foxes'},{name: 'Albert',score: 71,class: 'wolves'},{name: 'Viktor',score: 80,class: 'tigers'}]));
+ 	// {
+ 		// foxes: { name: 'Alexander', score: 100 },
+ 		// wolves: { name: 'Alisa', score: 76 },
+ 		// tigers: { name: 'Viktor', score: 80 }
+ 		// }
+ 		
 console.log(highestScore([])); 
 //{}
